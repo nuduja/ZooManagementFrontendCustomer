244096247b67e7bd3bcd28e034cbfacb9a5aa773
nuduja <nudujabandara@gmail.com>
2024-02-06 21:28:50 +0530

    Signup functions added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
+import Home from './pages/home';
 import Login from './pages/login';
+import Signup from './pages/signup';
 
 function App() {
     return (
         <Router>
             {/* <Navbar/> */}
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
             </Routes>
         </Router>
     );
